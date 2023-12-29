975411cd5df0b5fd6f3fec07fc2634233e6ae6b2
Mayurisharmaa <153805288+Mayurisharmaa@users.noreply.github.com>
2023-12-29 14:40:34 +0530

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,78 +1,208 @@
-/*--------------------- Copyright (c) 2023 -----------------------
-[Master Javascript]
-Project: bootstrap package
-Version: 1.0.0
--------------------------------------------------------------------*/
+const body= document.querySelector("body");
+const darkLight = document.querySelector("#darkLight");
+const sidebar = document.querySelector(".sidebar");
+const submenuItems = document.querySelectorAll(".submenu_item");
+const sidebarOpen = document.querySelector("#sidebarOpen");
+const sidebarClose = document.querySelector(".collapse_sidebar");
+const sidebarExpand = document.querySelector(".expand_sidebar");
+sidebarOpen.addEventListener("click", () => sidebar.classList.toggle("close"));
 
-  
-  
-$(document).ready(function(){
-  $(".openMenu").click(function(){
-    $(".myNav").addClass("active");
-  });
-  $(".hideMe").click(function(){
-      $(".myNav").removeClass("active");
-    });
-
-  
+sidebarClose.addEventListener("click", () => {
+  sidebar.classList.add("close", "hoverable");
+});
+sidebarExpand.addEventListener("click", () => {
+  sidebar.classList.remove("close", "hoverable");
 });
 
-  
-$(document).ready(function () {
-  $('.mainslider').owlCarousel({
-  items: 1,
-  nav: false,
-  
-  dots: false,
-  loop: true,
-  autoplay: true,
-  autoplayTimeout: 4000,
-  
-  responsiveClass: true,
-  responsive: {
-      0: {
-          items: 1,
-          nav: false
-      },
-      600: {
-          items: 1,
-  
-      },
-      1000: {
-          items: 1,
-  
-  
-      }
+sidebar.addEventListener("mouseenter", () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.remove("close");
   }
-  
-  })
-  })
-  $(document).ready(function () {
-    $('.testimonails').owlCarousel({
-    nav: true,
-    margin:15,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    responsiveClass: true,
-    responsive: {
-        0: {
-            items: 1,
-            nav: false
-        },
-        600: {
-            items: 1,
-    
-        },
-        1000: {
-            items:3,
-        }
-    }               
-    })
-    })
-        // AOS Animation
-        AOS.init({
-            duration: 1200,
-        })
+});
+sidebar.addEventListener("mouseleave", () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.add("close");
+  }
+});
 
-    
+darkLight.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    document.setI;
+    darkLight.classList.replace("bx-sun", "bx-moon");
+  } else {
+    darkLight.classList.replace("bx-moon", "bx-sun");
+  }
+});
+
+submenuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("show_submenu");
+    submenuItems.forEach((item2, index2) => {
+      if (index !== index2) {
+        item2.classList.remove("show_submenu");
+      }
+    });
+  });
+});
+
+if (window.innerWidth < 768) {
+  sidebar.classList.add("close");
+} else {
+  sidebar.classList.remove("close");
+}
+
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+
+// 
+const inputs = document.querySelectorAll("input"),
+  button = document.querySelector("button");
+
+// iterate over all inputs
+inputs.forEach((input, index1) => {
+  input.addEventListener("keyup", (e) => {
+    // This code gets the current input element and stores it in the currentInput variable
+    // This code gets the next sibling element of the current input element and stores it in the nextInput variable
+    // This code gets the previous sibling element of the current input element and stores it in the prevInput variable
+    const currentInput = input,
+      nextInput = input.nextElementSibling,
+      prevInput = input.previousElementSibling;
+
+    // if the value has more than one character then clear it
+    if (currentInput.value.length > 1) {
+      currentInput.value = "";
+      return;
+    }
+    // if the next input is disabled and the current value is not empty
+    //  enable the next input and focus on it
+    if (nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== "") {
+      nextInput.removeAttribute("disabled");
+      nextInput.focus();
+    }
+
+    // if the backspace key is pressed
+    if (e.key === "Backspace") {
+      // iterate over all inputs again
+      inputs.forEach((input, index2) => {
+        // if the index1 of the current input is less than or equal to the index2 of the input in the outer loop
+        // and the previous element exists, set the disabled attribute on the input and focus on the previous element
+        if (index1 <= index2 && prevInput) {
+          input.setAttribute("disabled", true);
+          input.value = "";
+          prevInput.focus();
+        }
+      });
+    }
+    //if the fourth input( which index number is 3) is not empty and has not disable attribute then
+    //add active class if not then remove the active class.
+    if (!inputs[3].disabled && inputs[3].value !== "") {
+      button.classList.add("active");
+      return;
+    }
+    button.classList.remove("active");
+  });
+});
+
+//focus the first input which index is 0 on window load
+window.addEventListener("load", () => inputs[0].focus());
+
+
+function changeText() {
+  var y = document.getElementById("input-file").value;
+  document.getElementById("selectedFileName").innerHTML = y;
+}
+
+var app = {
+  settings: {
+    container: $('.calendar'),
+    calendar: $('.front'),
+    days: $('.weeks span'),
+    form: $('.back'),
+    input: $('.back input'),
+    buttons: $('.back button')
+  },
+
+  init: function() {
+    instance = this;
+    settings = this.settings;
+    this.bindUIActions();
+  },
+
+  swap: function(currentSide, desiredSide) {
+    settings.container.toggleClass('flip');
+
+    currentSide.fadeOut(900);
+    currentSide.hide();
+    desiredSide.show();
+
+  },
+
+  bindUIActions: function() {
+    settings.days.on('click', function(){
+      instance.swap(settings.calendar, settings.form);
+      settings.input.focus();
+    });
+
+    settings.buttons.on('click', function(){
+      instance.swap(settings.form, settings.calendar);
+    });
+  }
+}
+
+app.init();
+
+const body = document.querySelector("body"),
+modeToggle = body.querySelector(".mode-toggle");
+sidebar = body.querySelector("nav");
+sidebarToggle = body.querySelector(".sidebar-toggle");
+
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
+body.classList.toggle("dark");
+}
+
+let getStatus = localStorage.getItem("status");
+if (getStatus && getStatus === "close") {
+sidebar.classList.toggle("close");
+}
+
+modeToggle.addEventListener("click", () => {
+body.classList.toggle("dark");
+if (body.classList.contains("dark")) {
+  localStorage.setItem("mode", "dark");
+} else {
+  localStorage.setItem("mode", "light");
+}
+});
+
+sidebarToggle.addEventListener("click", () => {
+sidebar.classList.toggle("close");
+if (sidebar.classList.contains("close")) {
+  localStorage.setItem("status", "close");
+} else {
+  localStorage.setItem("status", "open");
+}
+})
+
+
